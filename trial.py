a862aa364318d0e3a91a14538f7bc2b6bcb616f9
Pragati Singh <pragatisingh2107@gmail.com>
2019-07-25 11:12:47 +0000

    feature

--- trial.py
+++ trial.py
@@ -12,20 +12,19 @@ Auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
 Auth.set_access_token(Access_token, Access_token_secret)
 
 api = tweepy.API(Auth)
-public_tweets = api.search('chandrayaan2')
+#public_tweets = api.search('chandrayaan2')
 
 maximum_number_of_tweets_to_be_extracted = 1000
 
 # Mention the hashtag that you want to look out for
 listOfTweets=[]
-hashtag = 'chandrayaan2'
 keys=['Screen Name','User Name','Tweet Created At','Tweet Text','User Location','Retweet Count','Phone Type']
 
 with open('result.csv', 'a') as csvFile:
     dict_writer = csv.DictWriter(csvFile, fieldnames=keys)
     dict_writer.writeheader()
 
-    for tweet in tweepy.Cursor(api.search, q='#chandrayaan2 -filter:retweets', since = "2019-07-23", until = "2019-07-24", rpp=100, lang = "en", tweet_mode='extended').items(1500):
+    for tweet in tweepy.Cursor(api.search, q='#Modi -filter:retweets', since = "2019-07-23", until = "2019-07-24", rpp=100, lang = "en", tweet_mode='extended').items(1500):
         dict_ = {'Screen Name': str(tweet.user.screen_name),
                  'User Name': tweet.user.name.encode('utf-8'),
                  'Tweet Created At': str(tweet.created_at),
