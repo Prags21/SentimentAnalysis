8e6bd30d10a3ed3d3f0a72b563bc325af98a3e44
Pragati Singh <pragatisingh2107@gmail.com>
2019-08-14 08:33:55 +0000

    N2

--- Visualization/n2.py
+++ Visualization/n2.py
@@ -6,7 +6,7 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import warnings
-
+from random import randint
 warnings.filterwarnings("ignore")
 
 #df = pd.DataFrame({'number':['123','234','345'],'contactnumber':['234','345','123'],'callduration':[1,2,4]})
@@ -30,8 +30,6 @@ for tweet in df.iterrows():
         dict_={'RT':rt_source,
               'user':tweet[1]['Screen Name'],
               'RT_Count':tweet[1]['Retweet Count']}
-        #print(dict_)
-
         listOfTweets.append(dict_)
 df1 = pd.DataFrame(listOfTweets)
 #print(listOfTweets)
@@ -40,12 +38,15 @@ G = nx.from_pandas_edgelist(df1,'RT','user')
 # G = nx.from_pandas_edgelist(df,'number','contactnumber', edge_attr='callduration')
 #durations = [i['callduration'] for i in dict(G.edges).values()]
 labels = [i for i in dict(G.nodes).keys()]
+print(labels)
 labels = {i:i for i in dict(G.nodes).keys()}
+print(labels)
 
+print(a)
 fig, ax = plt.subplots(figsize=(12,5))
 pos = nx.spring_layout(G)
 nx.draw_networkx_nodes(G, pos, ax = ax, labels=True)
-nx.draw_networkx_edges(G, pos, width=3, ax=ax)
+nx.draw_networkx_edges(G, pos, width=a, ax=ax)
 _ = nx.draw_networkx_labels(G, pos, labels, ax=ax)
 # #df = pd.DataFrame({'number':['123','234','345'],'contactnumber':['234','345','123'],'callduration':[1,2,4]})
 # df = pd.read_csv('../r.csv', encoding = "ISO-8859-1")
