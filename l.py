760555ac12159465da09b23f3e33ed9dbd1aebce
Pragati Singh <pragatisingh2107@gmail.com>
2019-08-18 19:13:37 +0000

    added cond for unicode escape

--- l.py
+++ l.py
@@ -17,17 +17,14 @@ def wordcloud(tweets,col):
     
 wordcloud(tweets,'text')
 #preprocess text in tweets by removing links, @UserNames, blank spaces, etc.
+#preprocess text in tweets by removing links, @UserNames, blank spaces, etc.
 def preprocessing_text(table):
     #put everythin in lowercase
     table['Tweet Text'] = table['Tweet Text'].str.lower()
-    #for tweet in table['Tweet Text']:
-    #table['Tweet Text']=bytes(table['Tweet Text'], "utf-8").decode("unicode_escape")
-    #print(table['Tweet Text'])
-        #break
-    for item in table:
-        print(item)
-    for i, tweet in enumerate(item['Tweet Text'] for item in table): 
-        print(i,tweet)
+  
+    for ind,item in enumerate(table['Tweet Text']):
+        table.at[ind,'Tweet Text']= bytes(item, "utf-8").decode("unicode_escape")
+     
     #Replace rt indicating that was a retweet
     table['Tweet Text'] = table['Tweet Text'].str.replace('rt', '')
     #Replace occurences of mentioning @UserNames
