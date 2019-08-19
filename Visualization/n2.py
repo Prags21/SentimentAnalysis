1d4db298834233f7bb54d5139353281b0c5acc6e
Pragati Singh <pragatisingh2107@gmail.com>
2019-08-19 18:53:47 +0000

    Influencers graph

--- Visualization/n2.py
+++ Visualization/n2.py
@@ -7,25 +7,24 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import warnings
 from collections import *
+from itertools import count
 
 from random import randint
 warnings.filterwarnings("ignore")
 class OrderedCounter(Counter, OrderedDict):
     pass
-#df = pd.DataFrame({'number':['123','234','345'],'contactnumber':['234','345','123'],'callduration':[1,2,4]})
-df = pd.read_csv('/content/r.csv', encoding = "ISO-8859-1")
 listOfTweets=[]
 d =[]
-
+table=pd.read_csv('Dataset.csv', delimiter = ',',encoding = "utf-8")
 G = nx.DiGraph()
+dic_edge=[]
 def get_rt_sources(tweet):
     rt_patterns = re.compile(r"(RT|via)((?:\b\W*@\w+)+)", re.IGNORECASE)
     return [ source.strip()
              for tuple in rt_patterns.findall(tweet)
                  for source in tuple
                      if source not in ("RT", "via") ]
-for tweet in df.iterrows():
-    #print(df['Tweet Text'])
+for tweet in table.iterrows():
     t=str(tweet[1]['Tweet Text'])
     rt_sources = get_rt_sources(t)
     if not rt_sources: continue
@@ -35,31 +34,44 @@ for tweet in df.iterrows():
             d=re.split(r'@(\w+)' ,rt_source)
             for rt in d:
                 if (rt.isalnum() or '_' in rt):
-                    G.add_edges_from([(tweet[1]['Screen Name'],"@"+rt)], weight=tweet[1]['Retweet Count'])
+                    dic_edge.append((tweet[1]['Screen Name'],"@"+rt))
+                    #G.add_edges_from([(tweet[1]['Screen Name'],"@"+rt)], weight=tweet[1]['Retweet Count'])
                     listOfTweets.append("@"+rt)
         else :
-            G.add_edges_from([(tweet[1]['Screen Name'],rt_source)], weight=tweet[1]['Retweet Count'])
-            listOfTweets.append(rt_source)
-counts=Counter(listOfTweets).most_common(9)
+             dic_edge.append((tweet[1]['Screen Name'],rt_source))
+            #G.add_edges_from([(tweet[1]['Screen Name'],rt_source)], weight=tweet[1]['Retweet Count'])
+             listOfTweets.append(rt_source)
+counts=Counter(listOfTweets).most_common(15)
 a=[]
 size_n=[]
 labels = {}
+n_color=[]
+for x in dic_edge :
+  for item in counts:
+      if item[0] == x[1]:
+        G.add_edges_from([(x[0],x[1])])
+#print(G.edges())
+#print(f_edges)
 for i in counts:
-    a.append(i[0])
+    d={}
+    d['node_']=i[0]
+    d['frequency']=i[1]
+    a.append(d)
 for node in G.nodes():
-    if node in a:
-        #set the node name as the key and the label as its value
-        print(node)
-        size_n.append(160)
-        labels[node] = node
-    else:
-        size_n.append(10)
+    size_n.append(5)
+    n_color.append('blue')
+for node in G.nodes():
+    for idx, item in enumerate(a):
+        if node in item['node_']:
+            size_n[idx] = round(item['frequency']/10)
+            n_color[idx] = 'red'
+            labels[item['node_']] = item['node_']
+
 pos = nx.spring_layout(G)
 plt.figure(figsize=(12,12))
-
-_=nx.draw_networkx_nodes(G, pos, node_size=size_n,alpha = 0.7)
-_=nx.draw_networkx_edges(G, pos ,alpha=0.2 )
-_=nx.draw_networkx_labels(G,pos,labels,font_size=12,font_color='r')
+_=nx.draw_networkx_nodes(G, pos, node_size=size_n,node_color=n_color,alpha = 0.7)
+_=nx.draw_networkx_edges(G, pos ,alpha=0.2,edge_color='r' )
+_=nx.draw_networkx_labels(G,pos,labels,font_size=12,font_color='g')
 
 
 #nx.eigenvector_centrality_numpy(G)
